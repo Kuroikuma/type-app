4676cbc57d1276bf29fbb0d1e25a767a9d95dc28
Junior Ulisse Cabrera Hurtado <jhurtado@plannerapp.net>
2022-09-25 18:25:24 -0600

    Init React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface Sub{
+    nick: string;
+    subMount: number;
+    avatar: string;
+    descripcion?: string;
+}
+
+interface StateApp {
+    stateSub: Array<Sub>
+}
+
+
+
+const INITIAL_STATE = [
+    {
+        nick: "dapelu",
+        subMount: 3,
+        avatar: "https://i.pravatar.cc/150?u=dapelu",
+        descripcion: "Dapelu hace de moderador algunas ocaciones"
+    },
+    {
+        nick: "sergio serrano",
+        subMount: 7,
+        avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
+    }
+]
+
+const App = () => {
+
+  const [sub, setSub] = useState<StateApp["stateSub"]>([]);
+  
+  useEffect(() => {
+    setSub(INITIAL_STATE)
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
